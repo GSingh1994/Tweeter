e25019538bb976172bd66c5578d03155c09cab14
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-04-27 09:08:32 -0500

    Add loadTweets fn to make Ajax GET request to /tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,4 @@
 $().ready(() => {
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-    {
-      user: {
-        name: 'Newton',
-        avatars: 'https://i.imgur.com/73hZDYK.png',
-        handle: '@SirIsaac',
-      },
-      content: {
-        text: 'If I have seen further it is by standing on the shoulders of giants',
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: 'Descartes',
-        avatars: 'https://i.imgur.com/nlhLi3I.png',
-        handle: '@rd',
-      },
-      content: {
-        text: 'Je pense , donc je suis',
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
   const createTweetElement = (tweetData) => {
     // return tweet article
     return $(`
@@ -51,9 +25,16 @@ $().ready(() => {
     });
   };
 
-  $('#tweet-form').submit((event) => {
-    event.preventDefault();
+  $('#tweet-form').submit(function (e) {
+    e.preventDefault();
+    const data = $(this).serialize();
+    $.post('/tweets', data);
   });
 
-  renderTweets(data);
+  const loadTweets = () => {
+    $.ajax('/tweets', { method: 'GET' }).then((tweetData) => {
+      renderTweets(tweetData);
+    });
+  };
+  loadTweets();
 });
