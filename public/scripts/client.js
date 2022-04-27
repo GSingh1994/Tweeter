90e2928c955474496861d589b48b0e7c5d3095fb
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-04-27 15:37:00 -0500

    Add function to escape chars to prevent XSS attack

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,23 +1,31 @@
 $().ready(() => {
+  //escape chars to prevent XSS attack
+  const escape = (str) => {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
   const createTweetElement = (tweetData) => {
     const formatDate = timeago.format(tweetData.created_at);
     // return tweet article
     return $(`
-    <article class="tweet">
-      <header>
-        <div class="user">
-          <img class="avatar" src=${tweetData.user.avatars} alt="avatar of user" />
-          <div class="user-name">${tweetData.user.name}</div>
-        </div>
-        <span class="user-handle">${tweetData.user.handle}</span>
-      </header>
-      <p class="content">${tweetData.content.text}</p>
-      <hr />
-      <footer>
-        <div class="time">${formatDate}</div>
-        <div class="icon-tray"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></div>
-      </footer>
-    </article>`);
+      <article class="tweet">
+        <header>
+          <div class="user">
+            <img class="avatar" src=${tweetData.user.avatars} alt="avatar of user" />
+            <div class="user-name">${tweetData.user.name}</div>
+          </div>
+          <span class="user-handle">${tweetData.user.handle}</span>
+        </header>
+        <p class="content">${escape(tweetData.content.text)}</p>
+        <hr />
+        <footer>
+          <div class="time">${formatDate}</div>
+          <div class="icon-tray"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></div>
+        </footer>
+      </article>
+    `);
   };
 
   const renderTweets = (tweets) => {
@@ -34,7 +42,6 @@ $().ready(() => {
     if (tweetValue === null || tweetValue.length === 0) {
       return alert("Tweet can't be empty");
     }
-
     const data = $(this).serialize();
     $.post('/tweets', data).then(() => {
       e.target.reset();
