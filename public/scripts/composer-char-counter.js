e94f3500d982c0ddefc95e9acd44432418df869d
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-04-30 17:05:51 -0500

    Refactor input event-listener to make it more readable

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,20 @@
-// run this file after html is loaded
 $().ready(() => {
-  $('#tweet-text').on('input', function () {
-    let wordLimit = 140;
-    let $counter = $(this).parent().find('.counter');
-
-    //decrease counter on every char.
-    wordLimit -= $(this).val().length;
-    $counter.val(wordLimit);
-
-    // hide error message if any
-    $('#error-msg').slideUp('fast');
-    $counter.removeClass('red-counter');
-
-    if (parseInt($counter.val()) < 0) {
-      $counter.addClass('red-counter');
-    }
-  });
+  $('#tweet-text').on('input', onInput);
 });
+
+const onInput = function () {
+  let wordLimit = 140;
+  let $counter = $(this).parent().find('.counter');
+
+  //decrease counter on every char.
+  wordLimit -= $(this).val().length;
+  $counter.val(wordLimit);
+
+  // hide error message if any
+  $('#error-msg').slideUp('fast');
+  $counter.removeClass('red-counter');
+
+  if (parseInt($counter.val()) < 0) {
+    $counter.addClass('red-counter');
+  }
+};
